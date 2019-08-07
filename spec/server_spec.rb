9511764bd1995c4940eaf4b34c72a0796f642d9a
spikeburton <ralphburton3@gmail.com>
2019-08-07 15:16:17 -0400

    configure rack test

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -1,10 +1,16 @@
-require 'rack/test'
 require_relative '../server'
 
-describe 'App' do
+RSpec.describe 'App' do
   include Rack::Test::Methods
 
   def app
     App.new
+  end
+
+  it 'fetches people from SalesLoft API' do
+    get '/api/people'
+
+    # expect(last_response.body).to include("Hello World!")
+    expect(last_response).to be_ok
   end
 end
