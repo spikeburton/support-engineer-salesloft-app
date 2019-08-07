7ad74bd2b98f1eb06f6dd57f2d6dc1dbc96edbf7
spikeburton <ralphburton3@gmail.com>
2019-08-07 17:52:59 -0400

    add heroku build script for client

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+fetch('/api/people')
+  .then(res => res.json())
+  .then(console.log);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
